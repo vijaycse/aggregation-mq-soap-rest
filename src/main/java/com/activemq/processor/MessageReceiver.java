61b6141d27f86fdc76398900caef3d837108d208
vijay ram <vijay.ramcse@gmail.com>
2015-03-29 09:29:43 -0400

    initial commit

---
+++ src/main/java/com/activemq/processor/MessageReceiver.java
@@ -0,0 +1,10 @@
+package com.activemq.processor;
+
+
+	public class MessageReceiver {
+		public String getMessage(String str){
+			System.out.println("sting   "+str);
+			return str;
+		}
+
+	}
---
+++ src/main/java/com/activemq/processor/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author vijay
+ *
+ */
+package com.activemq.processor;
